668bc981147828b6f4ff7c342870941c10e46ff3
Taehoon Lee <ddaa0062@likelion.org>
2017-06-24 10:07:27 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,17 @@ class UsersController < ApplicationController
   end
 
   def create
-     @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-     redirect_to @user
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
-   private
+
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
