f6fc77b73bf110f6acf5cdfc6149bd015a54e999
Taehoon Lee <ddaa0062@likelion.org>
2017-06-24 12:55:20 +0000

    user-microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
   
-  def show
+ def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -51,16 +52,17 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    
-   def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-   end
- 
-   def correct_user
+
+    # Before filters
+
+    # Confirms the correct user.
+    def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
-   end
- end
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+end
